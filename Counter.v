212ae17367a5ef83887b92046b82ff8923268c8a
K_Oniszczuk <52426577+Torevus@users.noreply.github.com>
2020-08-03 20:26:39 +0200

    Update Counter.v

--- Counter.v
+++ Counter.v
@@ -6,22 +6,22 @@
 
 // (c) 2020 , Karol Oniszczuk
 
-module Counter( 						    // Module declaration, name and I/O
-    input Counter_Clock,			        // Clock 
-    input Counter_Reset,			        // Reset
-    output [7:0] Counter_Data           	// Output of data 8-bit width
+module Counter(                     // Module declaration, name and I/O
+    input Counter_Clock,            // Clock 
+    input Counter_Reset,            // Reset
+    output [7:0] Counter_Data       // Output of data 8-bit width
     );
 
-reg [7:0] Count; 					        // Eight bit register
+reg [7:0] Count;                    // Eight bit register
 
-always @(posedge Counter_Clock)             // Do always with rising edge of clock				 
-begin										// Begin of process
-  if(Counter_Reset)					        // If Reset equals 1
-    Count <= 8'd0;						    // Change "Count" to 0
-  else									    // In other case
-    Count <= Count + 8'd1;		            // Incrementation. The value goes from 0 to 255 (+1)
-end							 		        // End of process
+always @(posedge Counter_Clock)     // Do always with rising edge of clock				 
+begin                               // Begin of process
+  if(Counter_Reset)                 // If Reset equals 1
+    Count <= 8'd0;                  // Change "Count" to 0
+  else                              // In other case
+    Count <= Count + 8'd1;          // Incrementation. The value goes from 0 to 255 (+1)
+end                                 // End of process
 
-assign Counter_Data = Count;                // Data assignment
+assign Counter_Data = Count;        // Data assignment
 
 endmodule
